76ee33efc8e6e8696e824004ba3b6db5655ebdca
Nathan Johnson <johnsn27@mc-s115012.local>
2019-02-05 17:00:43 +0000

    Header UI

--- Lab5/my_shell_outline.py
+++ Lab5/my_shell_outline.py
@@ -15,8 +15,8 @@ from datetime import datetime
 # 7. up              - check not at the top of the directory tree - can't go up from root
 # 8. finish
 
-headers = ["File Name", "Last Change", "Type", "OwnerID", "Size in Bytes", "Executable"]  # column headers
-width = [20, 24, 8, 12, 20, 8]  # max width of data in each column
+headers = ["File Name", "Type", "Last Change", "OwnerID", "Size in Bytes", "Executable"]  # column headers
+width = [20, 8, 24, 12, 20, 8]  # max width of data in each column
 
 # ========================
 #  file command
@@ -25,7 +25,6 @@ width = [20, 24, 8, 12, 20, 8]  # max width of data in each column
 # ========================
 
 def info_cmd(fields):
-    info = []
     print_header(len(headers))
     for filename in os.listdir('.'): # move this to function and also call print_file_info from fileInfo2 from lab 4
         file_info(filename)
@@ -39,11 +38,11 @@ def info_cmd(fields):
 #    No arguments
 # ========================
 def files_cmd(fields):
+
     print_header(2)
-    files = []
     for filename in os.listdir('.'):
-        print(filename)
-        print(file_or_directory(filename))
+        files_cmd_info(filename)
+        print_file_info()
 # ----------------------
 # Other functions
 # ----------------------
@@ -61,21 +60,29 @@ def checkArgs(fields, num):
 def print_header(lengthOfHeader):
     field_num = 0
     output = ''
-    while field_num < len(headers):
+    while field_num < lengthOfHeader:
         output += '{field:{fill}<{width}}'.format(field=headers[field_num], fill=' ', width=width[field_num])
         field_num += 1
 
+    print(lengthOfHeader)
+    width_counter = 0
+    width_sum = 0
+    for x in width:
+        if width_counter == lengthOfHeader:
+            break
+        width_sum=width_sum+x
+        width_counter = width_counter+1
+
     print(output)
-    length = sum(width)
-    print('-' * length)
+    print('-' * width_sum)
 
 def file_info(name):
     global info
     info = []
     info.append(name)  # the file name
+    info.append(file_or_directory(name))
     modified = os.stat(name).st_mtime
     info.append(datetime.fromtimestamp(modified).strftime('%b %d %Y %H:%M:%S'))
-    info.append(file_or_directory(name))
     info.append(os.stat(name).st_uid)
     if os.path.isfile(name):
         info.append(os.stat(name).st_size)
@@ -99,10 +106,16 @@ def file_or_directory(name):
     else:
         return "file"
 
+def files_cmd_info(name):
+    global info
+    info = []
+    info.append(name)
+    info.append(file_or_directory(name))
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 while True:
-    line = raw_input("PShell>")  # NOTE! This is only for python 2. Should be 'input' for python 3
+    line = input("PShell>")  # NOTE! This is only for python 2. Should be 'input' for python 3
     fields = line.split()
     # split the command into fields stored in the fields list
     # fields[0] is the command name and anything that follows (if it follows) is an argument to the command
